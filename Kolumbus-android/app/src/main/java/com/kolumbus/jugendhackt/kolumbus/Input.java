05088c375c3006432c16f3c2840911bcf6667bd7
Rico Klimpel <rico@tklimpel.de>
2014-09-13 09:41:07 +0200

    activity trans

--- Kolumbus-android/app/src/main/java/com/kolumbus/jugendhackt/kolumbus/Input.java
+++ Kolumbus-android/app/src/main/java/com/kolumbus/jugendhackt/kolumbus/Input.java
@@ -1,9 +1,12 @@
 package com.kolumbus.jugendhackt.kolumbus;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class Input extends Activity {
@@ -12,6 +15,16 @@ public class Input extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
+
+        Button btn_los = (Button)findViewById(R.id.btn_inputlos);
+
+        btn_los.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Input.this, Suggestions.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
--- Kolumbus-android/app/src/main/java/com/kolumbus/jugendhackt/kolumbus/Splash.java
+++ Kolumbus-android/app/src/main/java/com/kolumbus/jugendhackt/kolumbus/Splash.java
@@ -1,17 +1,30 @@
 package com.kolumbus.jugendhackt.kolumbus;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class Splash extends Activity {
 
+    private Handler mHandler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        mHandler.postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(Splash.this, Input.class);
+                startActivity(intent);
+            }
+        }, 5000);
+
+
     }
 
 
