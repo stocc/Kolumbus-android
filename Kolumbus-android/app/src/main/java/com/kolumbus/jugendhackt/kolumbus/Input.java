6f690157a40d60cd6bf7c9b5019af85e3b789c79
Rico Klimpel <rico@tklimpel.de>
2014-09-13 15:18:22 +0200

    now i will change allllll!!!! muhaha

--- Kolumbus-android/app/src/main/java/com/kolumbus/jugendhackt/kolumbus/Input.java
+++ Kolumbus-android/app/src/main/java/com/kolumbus/jugendhackt/kolumbus/Input.java
@@ -1,16 +1,25 @@
 package com.kolumbus.jugendhackt.kolumbus;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.RadioButton;
+
+import java.util.Calendar;
 
 
 public class Input extends Activity {
+
+    int mYear;
+    int mMonth;
+    int mDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +33,62 @@ public class Input extends Activity {
             public void onClick(View v) {
                 Intent intent = new Intent(Input.this, Suggestions.class);
                 startActivity(intent);
+
             }
         });
+
+        //Initialize Date Buttons
+        final EditText eT_DateStart = (EditText)findViewById(R.id.eT_dateStart);
+        final EditText eT_DateEnd = (EditText)findViewById(R.id.etDateEnd);
+
+/*
+        eT_DateStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                mYear = c.get(Calendar.YEAR);
+                mMonth = c.get(Calendar.MONTH);
+                mDay = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dpd = new DatePickerDialog(Input.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                eT_DateStart.setText(dayOfMonth + "-"
+                                        + (monthOfYear + 1) + "-" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                dpd.show();
+            }
+        });
+
+
+
+        eT_DateEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar c = Calendar.getInstance();
+                mYear = c.get(Calendar.YEAR);
+                mMonth = c.get(Calendar.MONTH);
+                mDay = c.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dpd = new DatePickerDialog(Input.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                eT_DateEnd.setText(dayOfMonth + "-"
+                                        + (monthOfYear + 1) + "-" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                dpd.show();
+            }
+        });
+*/
+
     }
 
 
